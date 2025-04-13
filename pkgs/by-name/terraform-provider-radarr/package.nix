d5f2b45aff738101ddf92d3b1d5ca1ea41b5f39c
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2025-04-13 16:26:10 +0200

    init: terraform-provider-radarr at 2.3.2

---
+++ pkgs/by-name/terraform-provider-radarr/package.nix
@@ -0,0 +1,20 @@
+{
+  terraform-providers,
+  lib,
+  homepage ? "https://registry.terraform.io/providers/devopsarr/radarr",
+  provider-source-address ? null,
+  ...
+}:
+terraform-providers.mkProvider {
+  inherit homepage;
+  owner = "devopsarr";
+  repo = "terraform-provider-radarr";
+  rev = "v2.3.2";
+  version = "2.3.2";
+  hash = "sha256-IsyvSGeLOBsjt1rhPmvOiJYI2FeolPjEiR0/IODqiTU=";
+  vendorHash = "sha256-/7oKRDj40N/LXB5Z3KeASie9IwnUMi1nYiFpwpqxNys=";
+  spdx = "MIT";
+}
+// lib.optionalAttrs (provider-source-address != null) {
+  inherit provider-source-address;
+}
